bbd79cc53d2100ddd1cf59c1d1b177b16f7e4358
anya96k <161941144+anya96k@users.noreply.github.com>
2024-04-25 11:05:02 +0530

    Add files via upload

---
+++ exp10 usins fri fun 2 class.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+class B;
+class A
+{
+	
+    int x;  
+    public:  
+    void setdata(int i)  
+    {  
+        x=i;  
+    }  
+    friend void min(A,B); 
+};
+class B
+{
+	int y;
+	public:
+		void setdata(int i)
+		{
+		y=i;	
+		}
+		friend void min(A,B);
+};
+void min(A a,B b)  
+{  
+    if(a.x<=b.y)  
+ cout << a.x << endl;  
+    else  
+    cout << b.y << endl;  
+}  
+   int main()  
+{  
+   A a;  
+   B b;  
+   a.setdata(1110);  
+   b.setdata(1111);  
+   min(a,b);  
+    return 0;  
+ }
